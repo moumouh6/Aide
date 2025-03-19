17e2e28634743f2c30bdbd3618a677e1e7fca08d
Alem Mohamed <126617660+moumouh6@users.noreply.github.com>
2025-03-19 17:29:35 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -4,8 +4,3 @@
     function toggleNav() {
         document.getElementById("sidebar").classList.toggle("active"); // Ajouter ou supprimer la classe active
     }
-
-document.querySelectorAll(".site-mzl").forEach(button => {
-    button.addEventListener("click", () => {
-        alert("cette page n'est pas accessible pour le moment!");
-    });})
